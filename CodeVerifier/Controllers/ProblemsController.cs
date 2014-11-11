09403b866a44e57a8f0210b71846ffc38f4459c6
syhestkiy <syhestkiy@gmail.com>
2014-11-11 19:55:43 +0200

    ProblemController
    Problem controller added, small changes in _Layout page

---
+++ CodeVerifier/Controllers/ProblemsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Security;
+using CodeVerifier.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace CodeVerifier.Controllers
+{
+    public class ProblemsController : Controller
+    {
+        private MaidDbContext db = new MaidDbContext();
+        // GET: Problems
+        public ActionResult Index()
+        {
+            //todo fix this problem
+            //bool hasPermission = false;
+            //if (GetCurrentUser() != null)
+            //    hasPermission = true;
+            //ViewBag.HasPermission = hasPermission;
+            return View(db.Problems.ToList());
+        }
+
+        // GET: Problems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Problem problem = db.Problems.Find(id);
+            if (problem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(problem);
+        }
+
+        // GET: Problems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Problems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Task,InputExample,OutputExample")] Problem problem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Problems.Add(problem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(problem);
+        }
+
+        // GET: Problems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Problem problem = db.Problems.Find(id);
+            if (problem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(problem);
+        }
+
+        // POST: Problems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Task,InputExample,OutputExample")] Problem problem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(problem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(problem);
+        }
+
+        // GET: Problems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Problem problem = db.Problems.Find(id);
+            if (problem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(problem);
+        }
+
+        // POST: Problems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Problem problem = db.Problems.Find(id);
+            db.Problems.Remove(problem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private MembershipUser GetCurrentUser()
+        {
+            MembershipUser user;
+            //can be exception here
+            try
+            {
+                user = Membership.GetUser(User.Identity.Name);
+                Guid currentUserID = (Guid) user.ProviderUserKey;
+            }
+            catch (Exception)
+            {
+                user=null;
+            }
+            
+           return user;
+        }
+    }
+}
---
+++ CodeVerifier/Models/MaidDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace CodeVerifier.Models
+{
+    public class MaidDbContext:DbContext
+    {
+        public DbSet<Problem> Problems { get; set; }
+        public DbSet<ProblemSolution> ProblemSolutions { get; set; } 
+    }
+}
