baf7ce7c1c7721c0714b23fefcbdb4114741802c
syhestkiy <syhestkiy@gmail.com>
2014-11-11 16:53:20 +0200

    Model classes added
    Problem
    Problem solution

--- CodeVerifier/Models/Problem.cs
+++ CodeVerifier/Models/Problem.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CodeVerifier.Models
+﻿namespace CodeVerifier.Models
 {
     public class Problem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Task { get; set; }
+        public string InputExample { get; set; }
+        public string OutputExample { get; set; }
     }
 }
---
+++ CodeVerifier/Models/ProblemSolution.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeVerifier.Models
+{
+    public class ProblemSolution
+    {
+        public int Id { get; set; }
+        public int ProblemId { get; set; }
+        public string SolutionCode { get; set; }
+    }
+}
